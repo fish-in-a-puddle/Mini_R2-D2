5f7b254dd174518bc8e2f79710fc3f5e25903a93
just-a-fish <egtoms314@gmail.com>
2026-07-03 14:36:52 +0000

    Updated setup of remote sketch

--- remote-controlled-v0.1.ino
+++ remote-controlled-v0.1.ino
@@ -1,11 +1,27 @@
 #include <irRemote.h>
 
-int recv_pin = 3;
-IRrecv irrecv(recv_pin);
+const int steeringPin = 9;
+const int motorControl = 8;
+const int irPin = 3;
+const int headPin = 5;
+const int blueLed = 13;
+const int redLed = 12;
+const int usTrig = 10;
+const int usEcho = 11;
+const int buzzer = 7;
+
+IRrecv irrecv(irPin);
 decode_results results;
 
 void setup() {
     irrecv.enableIRIn();
+    pinMode(motorControl, OUTPUT);
+    pinMode(blueLed, OUTPUT);
+    pinMode(redLed, OUTPUT)
+    steeringServo.attach(steeringPin);
+    headServo.attach(headPin);
+    steeringServo.write(90);
+    headServo.write(90);
 
 }
 
@@ -15,7 +31,7 @@ void loop() {
         ir_value = (results.value, HEX);
         irrecv.resume();
         case (ir_value == ) {
-            
+
         }
     }
 }
