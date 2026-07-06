6aae5f032fdaee5e35f2c127610161bcd2b4bfe6
just-a-fish <egtoms314@gmail.com>
2026-07-06 20:11:50 +0000

    Updated code for motor driver

--- remote-controlled-v0.1e.ino
+++ remote-controlled-v0.1e.ino
@@ -1,25 +1,30 @@
-#include <irRemote.hpp>
+#include <IRremote.hpp>
 #include <Servo.h>
 //IRremote library v4.4.0 is required
 
-
+// Establish pins
 const int steeringPin = 9;
-const int motorControl = 8;
+const int motorPlus = 8;
+const int motorMinus = 7;
 const int irPin = 3;
 const int headPin = 5;
 const int blueLed = 13;
 const int redLed = 12;
 const int usTrig = 10;
 const int usEcho = 11;
-const int buzzer = 7;
+const int buzzer = 4;
 
 uint64_t decCode;
+Servo headServo, steeringServo;
 
 void setup() {
     IrReceiver.begin(irPin);
-    pinMode(motorControl, OUTPUT);
+    pinMode(motorPlus, OUTPUT);
+    pinMode(motorMinus, OUTPUT);
     pinMode(blueLed, OUTPUT);
-    pinMode(redLed, OUTPUT)
+    pinMode(redLed, OUTPUT);
+    digitalWrite(motorPlus, LOW);
+    digitalWrite(motorPlus, LOW);
     steeringServo.attach(steeringPin);
     headServo.attach(headPin);
     steeringServo.write(90);
@@ -35,12 +40,24 @@ void loop() {
 
     if(decCode == 7477 || decCode == 5429)  
     {
-       digitalWrite(blueLed, HIGH);
+      steeringServo.write(90);
+       digitalWrite(motorPlus, HIGH);
+       digitalWrite(motorMinus, LOW);
+       IrReceiver.resume();
     }
     else if(decCode == 7478 || decCode == 5430) {
-        digitalWrite(blueLed, LOW);
+        digitalWrite(motorPlus, LOW);
+        digitalWrite(motorPlus, LOW);
+        IrReceiver.resume();
     }
-    IrReceiver.resume();  // Receive the next value
+    else if(decCode == 7457 || decCode == 5409) {
+      steeringServo.write(45);
+      IrReceiver.resume();
+    }
+    else if (decCode == 7456 || decCode == 5408) {
+      steeringServo.write(135);
+      IrReceiver.resume();
+    }
   }  
   
 }
